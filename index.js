92c10fe20bfce35a1c8da2ee012f3b36d5d7065f
TheGU <neotrez@gmail.com>
2014-10-18 17:48:06 +0700

    Poll from with angular scope and submit to save to local mongodb

--- index.js
+++ index.js
@@ -1,27 +1,113 @@
 // Init =================
 var express = require('express')
 var app = express(); // handle url request
-//var mongoose = require('mongoose'); // mongoose for mongodb
+var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
+var mongoose = require('mongoose'); // mongoose for mongodb
 //var morgan = require('morgan'); // log requests to the console (express4)
 
 // Config =================
-//mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'); // connect to mongoDB database
+mongoose.connect('mongodb://localhost:27017/hcr'); // connect to mongoDB database
+// poll model =================
+var PollSchema = new mongoose.Schema({
+        poll_user : { type: String, default: '' },
+        poll_date : { type: Date, default: Date.now },
 
-app.set('port', (process.env.PORT || 5000))
+        poll_1_1: { type: Boolean, default: false },
+        poll_1_2: { type: Boolean, default: false },
+        poll_1_3: { type: Boolean, default: false },
+        poll_1_4: { type: Boolean, default: false },
+        poll_1_5: { type: Boolean, default: false },
+        poll_1_6: { type: Boolean, default: false },
+        poll_1_7: { type: Boolean, default: false },
+        poll_1_8: { type: Boolean, default: false },
+        poll_1_9: { type: Boolean, default: false },
+        poll_1_10: { type: Boolean, default: false },
+        poll_1_11: { type: Boolean, default: false },
+        poll_1_12: { type: Boolean, default: false },
+        poll_1_12_text: { type: String, default: '' },
+    
+        poll_2: { type: String, default: '' },
+        poll_2_1_1: { type: Boolean, default: false },
+        poll_2_1_1_text: { type: String, default: '' },
+        poll_2_1_2: { type: Boolean, default: false },
+        poll_2_1_2_text: { type: String, default: '' },
+        poll_2_1_3: { type: Boolean, default: false },
+        poll_2_1_3_text: { type: String, default: '' },
+        poll_2_1_4: { type: Boolean, default: false },
+        poll_2_1_4_text: { type: String, default: '' },
+        poll_2_1_5: { type: Boolean, default: false },
+        poll_2_1_5_text: { type: String, default: '' },
+        poll_2_2_1: { type: Boolean, default: false },
+        poll_2_2_2: { type: Boolean, default: false },
+        poll_2_2_3: { type: Boolean, default: false },
+        poll_2_2_3_text: { type: String, default: '' },
+    
+        poll_3: { type: String, default: '' },
+    
+        poll_4_1: { type: Boolean, default: false },
+        poll_4_2: { type: Boolean, default: false },
+        poll_4_3: { type: Boolean, default: false },
+        poll_4_4: { type: Boolean, default: false },
+        poll_4_5: { type: Boolean, default: false },
+    
+        poll_5_1: { type: String, default: 0 },
+        poll_5_2: { type: String, default: 0 },
+        poll_5_3: { type: String, default: 0 },
+        poll_5_4: { type: String, default: 0 },
+        poll_5_5: { type: String, default: 0 },
+        poll_5_6: { type: String, default: 0 },
+        poll_5_7: { type: String, default: 0 },
+        poll_5_8: { type: String, default: 0 },
+        poll_5_9: { type: String, default: 0 },
+        poll_5_10: { type: String, default: 0 },
+        poll_5_10_text: { type: String, default: '' },
+    
+        poll_6_1_start: { type: String, default: '' },  //{type: [Number], index: '2d'},
+        poll_6_1_stop: { type: String, default: '' }, //{type: [Number], index: '2d'},
+        poll_6_2: { type: String, default: 0 },
+        poll_6_3: { type: String, default: 0 },
+    
+        poll_7_1: { type: Boolean, default: false },
+        poll_7_2: { type: Boolean, default: false },
+        poll_7_3: { type: Boolean, default: false },
+        poll_7_4: { type: Boolean, default: false },
+        poll_7_5: { type: Boolean, default: false },
+        poll_7_6: { type: Boolean, default: false },
+        poll_7_6_text: { type: String, default: '' }
+    });
+var Poll = mongoose.model('Poll', PollSchema);
+
 app.use(express.static(__dirname + '/public')) // set the static files location /public
+app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+//app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 //app.use(morgan('dev')); // log every request to the console
 
-// Core =================
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/index.html');
+// Routes =================
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
 })
 
-app.get('/poll', function(request, response) {
-  response.sendFile(__dirname + '/public/poll.html');
+app.get('/poll', function(req, res) {
+  res.sendFile(__dirname + '/public/poll.html');
 })
 
+// API =================
+app.get('/api/poll', function(req, res) {
+})
+
+app.post('/api/poll', function(req, res) {
+    Poll.create(req.body, function(err, poll) {
+        if (err)
+            res.send(err);
+        res.json(poll);
+    });
+})
 
 // Server =================
-app.listen(app.get('port'), function() {
-  console.log("HCR app is running on port " + app.get('port'))
+var server = app.listen(5000, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('HCR app is running at http://%s:%s', host, port)
 })
+
--- public/js/poll.js
+++ public/js/poll.js
@@ -2,12 +2,17 @@
     var module = angular.module("hcr-poll-collector", ["google-maps"]);
 }());
 
-function PollController($scope, $timeout, $log, $http, Logger) {
+function PollController($scope, $filter, $timeout, $log, $http, Logger) {
     Logger.doLog = true
     // Enable the new Google Maps visuals until it gets enabled by default.
     // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
     google.maps.visualRefresh = true;
 
+    $scope.status_msg = "";
+    $scope.poll = {
+        poll_date: $filter('date')(new Date, 'yyyy-MM-dd')
+    };
+    
     angular.extend($scope, {
         map: {
             control: {},
@@ -31,4 +36,29 @@ function PollController($scope, $timeout, $log, $http, Logger) {
             bounds: {}
         }
     });
+    
+    
+    $scope.submitPoll = function(){
+        $scope.status_msg = "Saving ... ";
+        console.log('Submit: ' + $scope.poll);
+        
+		$http.post('/api/poll', $scope.poll)
+			.success(function(data) {
+                $scope.poll = {
+                    poll_date: $filter('date')(new Date, 'yyyy-MM-dd')
+                }; // clear the form so our user is ready to enter another
+				$scope.status = data;
+				console.log(data);
+                $scope.status_msg = "Done";
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+                $scope.status_msg = 'Error: ' + data;
+			});        
+    };
+    $scope.resetPoll = function(){
+        $scope.poll = {
+            poll_date: $filter('date')(new Date, 'yyyy-MM-dd')
+        };     
+    };    
 }

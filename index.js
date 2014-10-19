9b1ddb59add40a37c8cedc81c80da4b79d4a5d8e
TheGU <neotrez@gmail.com>
2014-10-19 15:18:03 +0700

    - add button to add marker to center of map for location input - split poll schema to schema.js - upgrade to angular-google-maps-2.0.1.min.js

--- index.js
+++ index.js
@@ -5,81 +5,16 @@ var bodyParser = require('body-parser'); 	// pull information from HTML POST (ex
 var mongoose = require('mongoose'); // mongoose for mongodb
 //var morgan = require('morgan'); // log requests to the console (express4)
 
+var sc = require('./schema');
+
 // Config =================
 app.set('port', (process.env.PORT || 5000))
 var dbUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/hcr';
 
 mongoose.connect(dbUri); // connect to mongoDB database
 
-// poll model =================
-var PollSchema = new mongoose.Schema({
-        poll_user : { type: String, default: '' },
-        poll_date : { type: Date, default: Date.now },
-
-        poll_1_1: { type: Boolean, default: false },
-        poll_1_2: { type: Boolean, default: false },
-        poll_1_3: { type: Boolean, default: false },
-        poll_1_4: { type: Boolean, default: false },
-        poll_1_5: { type: Boolean, default: false },
-        poll_1_6: { type: Boolean, default: false },
-        poll_1_7: { type: Boolean, default: false },
-        poll_1_8: { type: Boolean, default: false },
-        poll_1_9: { type: Boolean, default: false },
-        poll_1_10: { type: Boolean, default: false },
-        poll_1_11: { type: Boolean, default: false },
-        poll_1_12: { type: Boolean, default: false },
-        poll_1_12_text: { type: String, default: '' },
-    
-        poll_2: { type: String, default: '' },
-        poll_2_1_1: { type: Boolean, default: false },
-        poll_2_1_1_text: { type: String, default: '' },
-        poll_2_1_2: { type: Boolean, default: false },
-        poll_2_1_2_text: { type: String, default: '' },
-        poll_2_1_3: { type: Boolean, default: false },
-        poll_2_1_3_text: { type: String, default: '' },
-        poll_2_1_4: { type: Boolean, default: false },
-        poll_2_1_4_text: { type: String, default: '' },
-        poll_2_1_5: { type: Boolean, default: false },
-        poll_2_1_5_text: { type: String, default: '' },
-        poll_2_2_1: { type: Boolean, default: false },
-        poll_2_2_2: { type: Boolean, default: false },
-        poll_2_2_3: { type: Boolean, default: false },
-        poll_2_2_3_text: { type: String, default: '' },
-    
-        poll_3: { type: String, default: '' },
-    
-        poll_4_1: { type: Boolean, default: false },
-        poll_4_2: { type: Boolean, default: false },
-        poll_4_3: { type: Boolean, default: false },
-        poll_4_4: { type: Boolean, default: false },
-        poll_4_5: { type: Boolean, default: false },
-    
-        poll_5_1: { type: String, default: 0 },
-        poll_5_2: { type: String, default: 0 },
-        poll_5_3: { type: String, default: 0 },
-        poll_5_4: { type: String, default: 0 },
-        poll_5_5: { type: String, default: 0 },
-        poll_5_6: { type: String, default: 0 },
-        poll_5_7: { type: String, default: 0 },
-        poll_5_8: { type: String, default: 0 },
-        poll_5_9: { type: String, default: 0 },
-        poll_5_10: { type: String, default: 0 },
-        poll_5_10_text: { type: String, default: '' },
-    
-        poll_6_1_start: { type: String, default: '' },  //{type: [Number], index: '2d'},
-        poll_6_1_stop: { type: String, default: '' }, //{type: [Number], index: '2d'},
-        poll_6_2: { type: String, default: 0 },
-        poll_6_3: { type: String, default: 0 },
-    
-        poll_7_1: { type: Boolean, default: false },
-        poll_7_2: { type: Boolean, default: false },
-        poll_7_3: { type: Boolean, default: false },
-        poll_7_4: { type: Boolean, default: false },
-        poll_7_5: { type: Boolean, default: false },
-        poll_7_6: { type: Boolean, default: false },
-        poll_7_6_text: { type: String, default: '' }
-    });
-var Poll = mongoose.model('Poll', PollSchema);
+// Model =================
+var Poll = mongoose.model('Poll', sc.PollSchema);
 
 app.use(express.static(__dirname + '/public')) // set the static files location /public
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
--- public/js/poll.js
+++ public/js/poll.js
@@ -1,20 +1,12 @@
-(function () {
-    var module = angular.module("hcr-poll-collector", ["google-maps"]);
-}());
-
-function PollController($scope, $filter, $timeout, $log, $http, Logger) {
-    Logger.doLog = true
-    // Enable the new Google Maps visuals until it gets enabled by default.
-    // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
-    google.maps.visualRefresh = true;
-
+angular.module('hcr-poll-collector', ['google-maps'.ns()])
+  .controller('PollController', function ($scope, $filter, $log, $timeout) {
+      
     $scope.status_msg = "";
     $scope.poll = {
         poll_date: $filter('date')(new Date, 'yyyy-MM-dd')
-    };
-    
-    angular.extend($scope, {
-        map: {
+    };      
+      
+    $scope.map = {
             control: {},
             version: "uknown",
             showTraffic: true,
@@ -28,16 +20,101 @@ function PollController($scope, $filter, $timeout, $log, $http, Logger) {
             options: {
                 streetViewControl: true,
                 panControl: true,
+                scrollwheel: true,
                 maxZoom: 20,
                 minZoom: 3
             },
             zoom: 12,
             dragging: false,
-            bounds: {}
-        }
+            bounds: {},
+            events: {
+                tilesloaded: function (map, eventName, originalEventArgs) {
+
+                },
+                click: function (mapModel, eventName, originalEventArgs) {
+                    // 'this' is the directive's scope
+                    $log.log("user defined event: " + eventName, mapModel, originalEventArgs);
+
+                    var e = originalEventArgs[0];
+                    var lat = e.latLng.lat(),
+                        lon = e.latLng.lng();
+                    $scope.clickInfo = {
+                        id: 0,
+                        title: 'Clicked here ' + 'lat: ' + lat + ' lon: ' + lon,
+                        latitude: lat,
+                        longitude: lon
+                    };
+                    $scope.$apply();
+                },
+                dragend: function () {
+                    self = this;
+                }
+            }   
+        };
+      
+      
+    // Marker control --------------------------
+    $scope.start_marker = {};
+    $scope.$watchCollection("start_marker.coords", function (newVal, oldVal) {
+      if (_.isEqual(newVal, oldVal))
+        return;
+      $scope.coordsUpdates++;
     });
-    
-    
+    $scope.setStartMarker = function(){
+        $scope.start_marker = {
+          id: 0,
+          coords: {
+            latitude: $scope.map.center.latitude, 
+            longitude: $scope.map.center.longitude
+          },
+          icon: "http://www.google.com/mapfiles/dd-start.png",
+          options: { 
+              draggable: true ,
+              labelContent: "จุดเริ่มต้น",
+              labelAnchor: "-15 30",
+              labelClass: "poll-marker-labels"
+          },
+          events: {
+            dragend: function (marker, eventName, args) {
+              var lat = marker.getPosition().lat();
+              var lon = marker.getPosition().lng();
+              $log.log('start marker dragend ' + lat + ',' + lon);
+            }
+          }
+        };        
+    };
+      
+    $scope.stop_marker = {};
+    $scope.$watchCollection("stop_marker.coords", function (newVal, oldVal) {
+      if (_.isEqual(newVal, oldVal))
+        return;
+      $scope.coordsUpdates++;
+    });
+    $scope.setStopMarker = function(){    
+        $scope.stop_marker = {
+          id: 1,
+          coords: {
+            latitude: $scope.map.center.latitude, 
+            longitude: $scope.map.center.longitude
+          },
+          icon: "http://www.google.com/mapfiles/dd-end.png",
+          options: { 
+              draggable: true ,
+              labelContent: "จุดสิ้นสุด",
+              labelAnchor: "-15 30",
+              labelClass: "poll-marker-labels"
+          },
+          events: {
+            dragend: function (marker, eventName, args) {
+              var lat = marker.getPosition().lat();
+              var lon = marker.getPosition().lng();
+              $log.log('หะนย marker dragend ' + lat + ',' + lon);
+            }
+          }
+        };          
+    };      
+            
+    // Submit and cancel form -------------------------
     $scope.submitPoll = function(){
         $scope.status_msg = "Saving ... ";
         console.log('Submit: ' + $scope.poll);
@@ -61,5 +138,8 @@ function PollController($scope, $filter, $timeout, $log, $http, Logger) {
         $scope.poll = {
             poll_date: $filter('date')(new Date, 'yyyy-MM-dd')
         };     
-    };    
-}
+    };         
+  
+  });
+
+

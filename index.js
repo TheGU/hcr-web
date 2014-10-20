d327d65f2144f5c3f755a59a72d3da99faada9ca
TheGU <neotrez@gmail.com>
2014-10-20 09:16:01 +0700

    reformat code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Init =================
-var express = require('express')
+var express = require('express');
 var app = express(); // handle url request
 var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
 var mongoose = require('mongoose'); // mongoose for mongodb
@@ -8,7 +8,7 @@ var mongoose = require('mongoose'); // mongoose for mongodb
 var sc = require('./schema');
 
 // Config =================
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 var dbUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/hcr';
 
 mongoose.connect(dbUri); // connect to mongoDB database
@@ -16,37 +16,37 @@ mongoose.connect(dbUri); // connect to mongoDB database
 // Model =================
 var Poll = mongoose.model('Poll', sc.PollSchema);
 
-app.use(express.static(__dirname + '/public')) // set the static files location /public
-app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(express.static(__dirname + '/public')); // set the static files location /public
+app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 //app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 //app.use(morgan('dev')); // log every request to the console
 
 // Routes =================
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-})
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
-app.get('/poll', function(req, res) {
-  res.sendFile(__dirname + '/public/poll.html');
-})
+app.get('/poll', function (req, res) {
+    res.sendFile(__dirname + '/public/poll.html');
+});
 
 // API =================
-app.get('/api/poll', function(req, res) {
-})
+app.get('/api/poll', function (req, res) {
+});
 
-app.post('/api/poll', function(req, res) {
-    Poll.create(req.body, function(err, poll) {
+app.post('/api/poll', function (req, res) {
+    Poll.create(req.body, function (err, poll) {
         if (err)
             res.send(err);
         res.json(poll);
     });
-})
+});
 
 // Server =================
-var server = app.listen(app.get('port'), function() {
-  var host = server.address().address
-  var port = server.address().port
-  console.log('HCR app is running at http://%s:%s', host, port)
+var server = app.listen(app.get('port'), function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('HCR app is running at http://%s:%s', host, port)
 })
 
--- public/js/poll.js
+++ public/js/poll.js
@@ -1,12 +1,12 @@
 angular.module('hcr-poll-collector', ['google-maps'.ns()])
-  .controller('PollController', function ($scope, $filter, $log, $timeout, $http) {
-      
-    $scope.status_msg = "";
-    $scope.poll = {
-        poll_date: $filter('date')(new Date, 'yyyy-MM-dd')
-    };      
-      
-    $scope.map = {
+    .controller('PollController', function ($scope, $filter, $log, $timeout, $http) {
+
+        $scope.status_msg = "";
+        $scope.poll = {
+            poll_date: $filter('date')(new Date, 'yyyy-MM-dd')
+        };
+
+        $scope.map = {
             control: {},
             version: "uknown",
             showTraffic: true,
@@ -37,114 +37,114 @@ angular.module('hcr-poll-collector', ['google-maps'.ns()])
                 dragend: function () {
                     self = this;
                 }
-            }   
+            }
         };
-      
-      
-    // Marker control --------------------------
-    $scope.start_marker = {};
-    $scope.$watchCollection("start_marker.coords", function (newVal, oldVal) {
-      if (_.isEqual(newVal, oldVal) || _.isEmpty(newVal))
-        return;
-      $scope.poll.poll_6_1_start.coordinates = [ newVal.longitude, newVal.latitude ];
-      $http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + newVal.latitude + ',' + newVal.longitude + '&sensor=true')			    .success(function(data) {
-				$log.log(data);
-                $scope.start_marker.description = data.results[0].formatted_address;
-			})
-			.error(function(data) {
-				$log.log('Error: ' + data);
-			});     
-    });
-    $scope.setStartMarker = function(){
-        $scope.poll.poll_6_1_start = {type:'Point',coordinates: []};
-        $scope.start_marker = {
-          id: 0,
-          description: '',
-          coords: {
-            latitude: $scope.map.center.latitude, 
-            longitude: $scope.map.center.longitude
-          },
-          icon: "http://www.google.com/mapfiles/dd-start.png",
-          options: { 
-              draggable: true ,
-              labelContent: "จุดเริ่มต้น",
-              labelAnchor: "-15 30",
-              labelClass: "poll-marker-labels"
-          },
-          events: {
-            dragend: function (marker, eventName, args) {
-              var lat = marker.getPosition().lat();
-              var lon = marker.getPosition().lng();
-              $log.log('start marker dragend ' + lat + ',' + lon);              
-            }
-          }
-        };        
-    };
-      
-    $scope.stop_marker = {};
-    $scope.$watchCollection("stop_marker.coords", function (newVal, oldVal) {
-      if (_.isEqual(newVal, oldVal) || _.isEmpty(newVal))
-        return;
-      $scope.poll.poll_6_1_stop.coordinates = [ newVal.longitude, newVal.latitude ];
-      $http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + newVal.latitude + ',' + newVal.longitude + '&sensor=true')			    .success(function(data) {
-				$log.log(data);
-                $scope.stop_marker.description = data.results[0].formatted_address;
-			})
-			.error(function(data) {
-				$log.log('Error: ' + data);
-			});     
-    });
-    $scope.setStopMarker = function(){    
-        $scope.poll.poll_6_1_stop = {type:'Point',coordinates: []};
-        $scope.stop_marker = {
-          id: 1,
-          description: '',
-          coords: {
-            latitude: $scope.map.center.latitude, 
-            longitude: $scope.map.center.longitude
-          },
-          icon: "http://www.google.com/mapfiles/dd-end.png",
-          options: { 
-              draggable: true ,
-              labelContent: "จุดสิ้นสุด",
-              labelAnchor: "-15 30",
-              labelClass: "poll-marker-labels"
-          },
-          events: {
-            dragend: function (marker, eventName, args) {
-              var lat = marker.getPosition().lat();
-              var lon = marker.getPosition().lng();
-              $log.log('stop marker dragend ' + lat + ',' + lon);
-            }
-          }
-        };          
-    };      
-            
-    // Submit and cancel form -------------------------
-    $scope.submitPoll = function(){
-        $scope.status_msg = "Saving ... ";   
-        $log.log($scope.poll);
-		$http.post('/api/poll', $scope.poll)
-			.success(function(data) {
-                $scope.resetPoll();
-				$scope.status = data;
-				$log.log(data);
-                $scope.status_msg = "Done";
-			})
-			.error(function(data) {
-				$log.log('Error: ' + data);
-                $scope.status_msg = 'Error: ' + data;
-			});        
-    };
-    
-    $scope.resetPoll = function(){
-        $scope.poll = {
-            poll_date: $filter('date')(new Date, 'yyyy-MM-dd')
-        };     
-        $scope.stop_marker = {};
+
+
+        // Marker control --------------------------
         $scope.start_marker = {};
-    };         
-  
-  });
+        $scope.$watchCollection("start_marker.coords", function (newVal, oldVal) {
+            if (_.isEqual(newVal, oldVal) || _.isEmpty(newVal))
+                return;
+            $scope.poll.poll_6_1_start.coordinates = [ newVal.longitude, newVal.latitude ];
+            $http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + newVal.latitude + ',' + newVal.longitude + '&sensor=true').success(function (data) {
+                $log.log(data);
+                $scope.start_marker.description = data.results[0].formatted_address;
+            })
+                .error(function (data) {
+                    $log.log('Error: ' + data);
+                });
+        });
+        $scope.setStartMarker = function () {
+            $scope.poll.poll_6_1_start = {type: 'Point', coordinates: []};
+            $scope.start_marker = {
+                id: 0,
+                description: '',
+                coords: {
+                    latitude: $scope.map.center.latitude,
+                    longitude: $scope.map.center.longitude
+                },
+                icon: "http://www.google.com/mapfiles/dd-start.png",
+                options: {
+                    draggable: true,
+                    labelContent: "จุดเริ่มต้น",
+                    labelAnchor: "-15 30",
+                    labelClass: "poll-marker-labels"
+                },
+                events: {
+                    dragend: function (marker, eventName, args) {
+                        var lat = marker.getPosition().lat();
+                        var lon = marker.getPosition().lng();
+                        $log.log('start marker dragend ' + lat + ',' + lon);
+                    }
+                }
+            };
+        };
+
+        $scope.stop_marker = {};
+        $scope.$watchCollection("stop_marker.coords", function (newVal, oldVal) {
+            if (_.isEqual(newVal, oldVal) || _.isEmpty(newVal))
+                return;
+            $scope.poll.poll_6_1_stop.coordinates = [ newVal.longitude, newVal.latitude ];
+            $http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + newVal.latitude + ',' + newVal.longitude + '&sensor=true').success(function (data) {
+                $log.log(data);
+                $scope.stop_marker.description = data.results[0].formatted_address;
+            })
+                .error(function (data) {
+                    $log.log('Error: ' + data);
+                });
+        });
+        $scope.setStopMarker = function () {
+            $scope.poll.poll_6_1_stop = {type: 'Point', coordinates: []};
+            $scope.stop_marker = {
+                id: 1,
+                description: '',
+                coords: {
+                    latitude: $scope.map.center.latitude,
+                    longitude: $scope.map.center.longitude
+                },
+                icon: "http://www.google.com/mapfiles/dd-end.png",
+                options: {
+                    draggable: true,
+                    labelContent: "จุดสิ้นสุด",
+                    labelAnchor: "-15 30",
+                    labelClass: "poll-marker-labels"
+                },
+                events: {
+                    dragend: function (marker, eventName, args) {
+                        var lat = marker.getPosition().lat();
+                        var lon = marker.getPosition().lng();
+                        $log.log('stop marker dragend ' + lat + ',' + lon);
+                    }
+                }
+            };
+        };
+
+        // Submit and cancel form -------------------------
+        $scope.submitPoll = function () {
+            $scope.status_msg = "Saving ... ";
+            $log.log($scope.poll);
+            $http.post('/api/poll', $scope.poll)
+                .success(function (data) {
+                    $scope.resetPoll();
+                    $scope.status = data;
+                    $log.log(data);
+                    $scope.status_msg = "Done";
+                })
+                .error(function (data) {
+                    $log.log('Error: ' + data);
+                    $scope.status_msg = 'Error: ' + data;
+                });
+        };
+
+        $scope.resetPoll = function () {
+            $scope.poll = {
+                poll_date: $filter('date')(new Date, 'yyyy-MM-dd')
+            };
+            $scope.stop_marker = {};
+            $scope.start_marker = {};
+        };
+
+    });
 
 
